e8f865ae01c77dd0588f4a0eec6296e02579104d
SS Ribeiro <luan@ssribeiro.com>
2018-06-07 21:50:04 -0300

    environment production change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:8080/api/',
-  update_server_status_refresh_rate: 30*1000, // 30 seconds
+  api: 'https://angst.one:8080/api/',
+  update_server_status_refresh_rate: 15*1000, // 15 seconds
 };
